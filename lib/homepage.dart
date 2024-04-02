b808cd4da68b4916ca4772056e4f29b81440e6c4
Saksham0205 <saksham252003@gmail.com>
2024-04-02 20:46:39 +0530

    Structure defined

---
+++ lib/homepage.dart
@@ -0,0 +1,117 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:wallpaper_app/fullscreen.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    fetchapi();
+  }
+
+  List images = [];
+  int page = 1;
+  String api = "AsHJPEVykI1vFwH70U4BWV6CFrxvMFzTmnp5hNRLRMtYZaKlh2yurm1A";
+
+  fetchapi() async {
+    await http.get(Uri.parse("https://api.pexels.com/v1/curated?per_page=80"),
+        headers: {
+          'Authorization':
+              'AsHJPEVykI1vFwH70U4BWV6CFrxvMFzTmnp5hNRLRMtYZaKlh2yurm1A'
+        }).then((value) {
+      Map result = jsonDecode(value.body);
+      setState(() {
+        images = result['photos'];
+      });
+    });
+  }
+
+  loadmore() async {
+    setState(() {
+      page = page + 1;
+    });
+    String url =
+        "https://api.pexels.com/v1/curated?per_page=80&page=" + page.toString();
+    await http.get(Uri.parse(url), headers: {
+      'Authorization':
+          'AsHJPEVykI1vFwH70U4BWV6CFrxvMFzTmnp5hNRLRMtYZaKlh2yurm1A'
+    }).then((value) {
+      Map result = jsonDecode(value.body);
+      setState(() {
+        images.addAll(result['photos']);
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Wallpapers",
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: GridView.builder(
+              itemCount: images.length,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  crossAxisSpacing: 2,
+                  childAspectRatio: 2 / 3,
+                  mainAxisSpacing: 2),
+              itemBuilder: (context, index) {
+                return InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => FullScreen(
+                          imageurl: images[index]['src']['large2x'],
+                        ),
+                      ),
+                    );
+                  },
+                  child: Container(
+                    color: Colors.black,
+                    child: Image.network(images[index]['src']['tiny'], fit: BoxFit.cover,),
+                  ),
+                );
+              },
+            ),
+          ),
+
+          InkWell(
+            onTap: loadmore,
+            child: Container(
+              height: 60,
+              width: double.infinity,
+              color: Colors.black,
+              child: Center(
+                child: Text(
+                  "Load More",
+                  style: TextStyle(color: Colors.white, fontSize: 24),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
